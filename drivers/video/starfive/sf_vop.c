bb101d04ef9dd4fd9581f8c14f354560beca3ef7
Maud Spierings <maud_spierings@hotmail.com>
2024-12-28 13:24:52 +0100

    video: starfive: Use the correct function to get display timings
    display_read_edid is not an exposed function, use display_read_timings
    instead, this function is also simpler.
    
    Signed-off-by: Maud Spierings <maud_spierings@hotmail.com>

--- drivers/video/starfive/sf_vop.c
+++ drivers/video/starfive/sf_vop.c
@@ -506,22 +506,15 @@ static int sf_display_init(struct udevice *dev, ulong fbbase, ofnode ep_node)
 		while (try_cnt-- > 0) {
 			ret = device_probe(disp);
 			if (!ret) {
-				ret = display_read_edid(disp, edid, sizeof(edid));
+				ret = display_read_timing(disp, &timing);
 				if (ret <= 0) {
-					pr_err("%s: Failed to read edid\n", __func__);
+					pr_err("%s: Failed to read timings\n", __func__);
 					ret = -EACCES;
 				}
-				else {
-					ret = edid_get_timing(edid, sizeof(edid), &timing, &panel_bits_per_colour);
-					if (ret)
-						pr_err("%s: Failed to get edid timing\n", __func__);
-					else {
 						if (timing.hactive.typ == 2256 && timing.vactive.typ == 1504)
 							gBuiltinLCDActive = true;
 						pr_err("Display output: %s\n", gBuiltinLCDActive ? "Build-in LCD" : "HDMI");
 						break;
-					}
-				}
 			}
 
 			mdelay(100);
